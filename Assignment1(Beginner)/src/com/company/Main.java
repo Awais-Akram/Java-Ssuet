afaa6e9513266f2d538068f440b6478cbef3719c
Awais-Akram <awaisakramcheema@gmail.com>
2017-03-21 07:20:16 +0500

    Lab Assignment 1

---
+++ Assignment1(Beginner)/src/com/company/Main.java
@@ -0,0 +1,120 @@
+package com.company;
+
+    import java.util.Scanner;                           //Adding this for input
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        //Exercise: 1
+
+        System.out.println("Hello World");
+
+/*
+
+        //Exercise: 2
+
+        int a = 10;
+        int b = 10;
+        double c = 12.5;
+        String d = "Java programming";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+        //Exercise: 3
+
+        String userName = "firstName";
+        userName = userName + " lastName";
+        System.out.println("print userName variable:"+userName);
+        //OutPut = print userName variable:firstName lastName
+
+        userName = "modified username";
+        System.out.println("print userName variable:"+userName);
+        //OutPut = print userName variable:modified username
+
+
+
+        //Exercise: 4
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter Value of A: ");
+        int a = scan.nextInt();
+        System.out.println("Please Enter Value of B: ");
+        int b = scan.nextInt();
+        int a1 = a + b;
+        int a2 = a - b;
+        int a3 = a * b;
+        double a4 = a / b;
+        System.out.println("\nA + B = " + a1);                   //For newLine we use \n
+        System.out.println("A - B = " + a2);
+        System.out.println("A x B = " + a3);
+        System.out.println("A / B = " + a4);
+
+
+
+        //Exercise: 5
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter Value of A: ");
+        float a = scan.nextFloat();
+        System.out.println("Please Enter Value of B: ");
+        float b = scan.nextFloat();
+        System.out.println("\nA + B");
+        System.out.println(a=a+b);
+        System.out.println("\nThe Value of A is = " + a);
+
+
+
+        //Exercise: 6
+
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please Enter Your Age: ");
+            int a = scan.nextInt();
+            if (a >= 18) {
+                System.out.println("You are Eligible to Vote, Please cast your Vote");
+            } else {
+                System.out.println("Sorry you are Underage, You are not Eligible to Vote");
+            }
+        }
+        catch (Exception Invalid){
+            System.out.println("Please Enter Valid Age....");
+        }
+
+
+
+        //Exercise: 7
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter a Number:");
+        int a = scan.nextInt();
+        if (a % 2 == 0) {
+            System.out.println("Your input number is 'Even'");
+        }
+        else {
+            System.out.println("Your input number is not Even");
+        }
+
+
+
+        //Exercise: 8
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter Any Number:");
+        int a = scan.nextInt();
+        if (a % 2 != 0) {
+            System.out.println("Your input number is 'Odd'");
+        }
+        else {
+            System.out.println("Your input number is not Odd");
+        }
+*/
+
+    }
+}
